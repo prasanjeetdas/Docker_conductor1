dede20e02e3bccbbe6f36165deba71ff5129dcda
prasanjeetdas <41261501+prasanjeetdas@users.noreply.github.com>
2021-05-27 12:20:09 +0530

    Add files via upload

---
+++ DockerFileDemo/src/test/java/com/example/newDockerFile/DockerFileDemo/DockerFileDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.newDockerFile.DockerFileDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerFileDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
